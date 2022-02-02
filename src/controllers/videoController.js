99ab1328dd75c7d7060712ff0963e4a9b9d4ca27
yl9517 <yl9517@naver.com>
2022-02-02 11:40:26 +0900

    html\(pug) 적용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,5 @@
-export const trending = (req, res) => res.send("HOME Page Videos");
-export const watch = (req, res) => res.send("Watch Video");
-export const edit = (req, res) => res.send("Edit Video");
+export const trending = (req, res) => res.render("home", {pageTitle:"Home"});
+export const see = (req, res) => res.render("watch", {pageTitle:"Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle:"Edit"});
+export const upload = (req, res) => res.send("upload Video");
+export const deleteVideo = (req, res) => res.send("delete Video");
