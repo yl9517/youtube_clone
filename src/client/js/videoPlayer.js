48cf55eb5dc5af47314f3d934b6b0b071068153a
yl9517 <yl9517@naver.com>
2022-07-04 21:19:58 +0900

    리팩토링: videoPlayer의 play버튼과 mute버튼 코드 단순화

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volumen = document.getElementById("volumen");
+const volumenRange = document.getElementById("volumen");
 
 console.log(video);
 const handlePlayClick = (e) => {
@@ -12,23 +12,19 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
 
 const handleMute = (e) => {
   if (video.muted) {
     //muted 되어있으면 음소거 풀기
     video.muted = false;
-    muteBtn.innerText = "Mute";
   } else {
     video.muted = true;
-    muteBtn.innerText = "Unmute";
   }
+  muteBtn.innerText = video.muted ? "Mute" : "Unmute";
+  volumenRange.value = video.muted ? 0 : 0.5;
 };
 
 playBtn.addEventListener("click", handlePlayClick); //재생
 muteBtn.addEventListener("click", handleMute); //소리 끄기
-video.addEventListener("pause", handlePause); //pause 상태면 play 텍스트 출력
-video.addEventListener("play", handlePlay); //Play 상태면 pause 텍스트 출력
