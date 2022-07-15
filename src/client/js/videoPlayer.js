90ddfda80bec3af4decdb3ec4e36304c5911c22b
yl9517 <yl9517@naver.com>
2022-07-15 20:44:31 +0900

    기능추가: timeline handle로 video 영상 시간 조절하기

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,7 @@ const muteBtn = document.getElementById("mute");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const volumenRange = document.getElementById("volumen");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5; //global volume
 video.volume = volumeValue;
@@ -52,16 +53,24 @@ const handleVolumeChange = (e) => {
 const formatTime = (second) =>
   new Date(second * 1000).toISOString().substring(11, 19);
 
+//비디오가 load된 후
 const handleLoadedMetadata = () => {
   currentTime.innerHTML = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
 };
 
+//비디오의 시간이 update 될 때마다
 const handleTimeUpdate = () => {
   currentTime.innerHTML = formatTime(Math.floor(video.currentTime));
 };
 
+const handleTimeChange = (e) => {
+  video.currentTime = e.target.value;
+};
+
 playBtn.addEventListener("click", handlePlayClick); //재생
 muteBtn.addEventListener("click", handleMute); //소리 끄기
-volumenRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadedmetadata", handleLoadedMetadata);
-video.addEventListener("timeupdate", handleTimeUpdate);
+volumenRange.addEventListener("input", handleVolumeChange); //볼륨 변경
+video.addEventListener("loadedmetadata", handleLoadedMetadata); //비디오 load
+video.addEventListener("timeupdate", handleTimeUpdate); //비디오 시간 update
+timeline.addEventListener("input", handleTimeChange); //시청시간 변경
