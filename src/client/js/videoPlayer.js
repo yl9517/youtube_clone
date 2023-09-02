c29b01ca13abadf2be5d0f10c3117b4e58285f2d
yl9517 <yl9517@naver.com>
2023-09-02 14:54:59 +0900

    feat: 영상화면 안의 마우스유무/움직임에 따라 class 부여

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -5,7 +5,12 @@ const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const volumenRange = document.getElementById("volumen");
 const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
+let controlsTimeout = null;
+let controlsMovementTimeout = null;
 let volumeValue = 0.5; //global volume
 video.volume = volumeValue;
 
@@ -68,9 +73,47 @@ const handleTimeChange = (e) => {
   video.currentTime = e.target.value;
 };
 
+const handleFullScreen = () => {
+  const fullScreen = document.fullscreenElement;
+  if (fullScreen) {
+    document.exitFullscreen();
+    fullScreenBtn.innerText = "Enter Full Screen";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenBtn.innerText = "Exit Full Screen";
+  }
+};
+
+const hideControls = () => videoControls.classList.remove("showing");
+
+const handleMouseMove = () => {
+  //화면에 3초 안에 들어오면
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+
+  //화면 안에서 계속 마우스 움직이고 있으면 계속 timeout 초기화하면서 다시 3초 세기
+  if (controlsMovementTimeout) {
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
+  }
+  videoControls.classList.add("showing");
+  //화면 안에서 마우스 멈추면 3초뒤 아이콘 사라지게
+  controlsMovementTimeout = setTimeout(hideControls, 3000);
+};
+
+const handleMouseLeave = () => {
+  //3초 뒤 사라지게
+  controlsTimeout = setTimeout(hideControls, 3000);
+};
+
 playBtn.addEventListener("click", handlePlayClick); //재생
 muteBtn.addEventListener("click", handleMute); //소리 끄기
 volumenRange.addEventListener("input", handleVolumeChange); //볼륨 변경
 video.addEventListener("loadedmetadata", handleLoadedMetadata); //비디오 load
 video.addEventListener("timeupdate", handleTimeUpdate); //비디오 시간 update
 timeline.addEventListener("input", handleTimeChange); //시청시간 변경
+fullScreenBtn.addEventListener("click", handleFullScreen);
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
