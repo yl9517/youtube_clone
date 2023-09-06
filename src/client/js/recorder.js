c43149a6cceb6b0efe372a66a1c24bf2ff0a3454
yl9517 <yl9517@naver.com>
2023-09-07 08:19:52 +0900

    [part14] feat: 썸네일 생성

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -8,22 +8,45 @@ let stream;
 let recorder;
 let videoFile;
 const handleDownload = async () => {
+  //파일 변환 webm -> mp4
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load();
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
 
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
-  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
-  const mp4Url = URL.createObjectURL(mp4Blob); //마법의 url 생성
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg"
+  ); //-ss = 특정 시간대
 
-  //링크 생성
+  const mp4File = ffmpeg.FS("readFile", "output.mp4"); //파일 읽기
+  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" }); //해당 type의 blob 생성
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+
+  const mp4Url = URL.createObjectURL(mp4Blob); //마법의 url 생성
+  const thumbUrl = URL.createObjectURL(thumbBlob);
+
   const date = formatDate();
+  //영상 링크 생성
   const a = document.createElement("a");
   a.href = mp4Url;
   a.download = `${date}_MyRecording.mp4`;
   document.body.appendChild(a);
   a.click();
+
+  //썸네일 링크 생성
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbUrl;
+  thumbA.download = `${date}_MyThumbnail.jpg`;
+  document.body.appendChild(thumbA);
+  thumbA.click();
 };
 
 const handleStop = () => {
