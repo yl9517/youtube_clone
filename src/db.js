42b34ca58464ed08fde7e9ab8bf6b175674a0014
이여름 <iyeoleum@iyeoleum-ui-MacBookAir.local>
2024-01-10 19:05:20 +0900

    오타 수정

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.DB_URL, {
 const db = mongoose.connection; //엑세스
 
 const handleOpen = () => console.log("Connected to DB!!");
-const handleError = () => console.log("DB Error", err);
+const handleError = (error) => console.log("DB Error", error);
 
 db.on("error", handleError); //에러뜰 때마다 호출
 db.once("open", handleOpen); //맨 처음 단 한번 호출
