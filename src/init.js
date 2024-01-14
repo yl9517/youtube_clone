8260f27ecb474ae891264574a4912fbbc7d00185
이여름 <iyeoleum@iyeoleum-ui-MacBookAir.local>
2024-01-14 10:37:08 +0900

    docs: console log 수정

--- src/init.js
+++ src/init.js
@@ -9,4 +9,4 @@ import app from "./server.js";
 const PORT = process.env.PORT || 4000;
 
 //server에서 설정 후 개방
-app.listen(PORT, console.log(`Server listening on port ${PORT}`)); // listen : 해당 port번호를 듣고있다가, request(요청)이 들어오면 작동
+app.listen(PORT, console.log(`Server listening on http://localhost:${PORT}`)); // listen : 해당 port번호를 듣고있다가, request(요청)이 들어오면 작동
